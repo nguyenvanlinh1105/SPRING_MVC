bf8d4f61e5d35e3c4a97cbed34f6118c5822c417
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2025-04-29 23:57:04 +0700

    use ManyToMany of Spring JPA to create relationship

--- pring-mvc/src/main/java/com/linhnguyen/entity/RoleEntity.java
+++ pring-mvc/src/main/java/com/linhnguyen/entity/RoleEntity.java
@@ -1,7 +1,11 @@
 package com.linhnguyen.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -13,6 +17,10 @@ public class RoleEntity extends BaseEntity {
 	
 	@Column(name="code")
 	private String code;
+	
+	@ManyToMany(mappedBy = "roles")
+	private List<UserEntity> users = new ArrayList<>();
+
 	
 
 	public String getName() {
@@ -29,6 +37,14 @@ public class RoleEntity extends BaseEntity {
 
 	public void setCode(String code) {
 		this.code = code;
+	}
+
+	public List<UserEntity> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<UserEntity> users) {
+		this.users = users;
 	}
 
 }
--- pring-mvc/src/main/java/com/linhnguyen/entity/UserEntity.java
+++ pring-mvc/src/main/java/com/linhnguyen/entity/UserEntity.java
@@ -1,12 +1,17 @@
 package com.linhnguyen.entity;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,7 +31,14 @@ public class UserEntity extends BaseEntity {
 	@Column(name="status")
 	private Integer status;
 
-	
+	@ManyToMany
+	@JoinTable(
+	    name = "user_role",
+	    joinColumns = @JoinColumn(name = "userid"),
+	    inverseJoinColumns = @JoinColumn(name = "roleid")
+	)
+	private List<RoleEntity> roles = new ArrayList<>();
+
 	public String getUserName() {
 		return userName;
 	}
@@ -59,8 +71,16 @@ public class UserEntity extends BaseEntity {
 		this.status = status;
 	}
 
-	
+	public List<RoleEntity> getRoles() {
+		return roles;
+	}
 
+	public void setRoles(List<RoleEntity> roles) {
+		this.roles = roles;
+	}
+
+	
+	
 	
 	
 }
